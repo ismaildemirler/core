5c4acc45e36bef7124675ff0f06dc531849449d3
ismail.demirler <ismail.demirler@tubitak.gov.tr>
2024-05-04 20:27:46 +0100

    First

---
+++ src/main/java/org/ismail/core/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package org.ismail.core.calculator;
+
+public abstract class Calculator {
+
+    abstract void RouteCalculator();
+
+    void BatteryCalculator() {
+        System.out.println("5 Hours");
+    }
+}
